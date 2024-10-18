c8dc7d14366b2bef1f83b41a3efea1ba911387aa
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-19 03:52:50 +0530

    Add files via upload

---
+++ JS9.js
@@ -0,0 +1,56 @@
+let q=100;
+let p=200;
+console.log(p+q);
+//var age =25;
+/*if(true)
+{
+    console.log(age);
+}*/
+
+/*function solve()
+{
+    let age=23;
+    console.log(age);
+}
+solve();*/
+
+/*var x=10;
+var x=20;
+console.log(x);*/
+
+/*  let keyword is dynamic
+let a=100;
+a='divya';
+a=true; 
+*/ 
+
+//const a=20;
+//a=30;
+//console.log(a);
+
+//let num=90;
+//console.log(num);
+
+//let name;
+//console.log(name);
+
+//let num=null;
+//console.log(num);
+
+//let a=10;
+//console.log(++a);
+//console.log(--a);
+//console.log(a++);
+
+/*let s;
+console.log(s=30);
+console.log(s+=3);//a=a+3*/
+
+//let a=20;
+//console.log(a-=10);//a=a-8ss
+
+//Ternary Operator
+let a=20;
+//let b=90;
+let status1=(a>=18)?'true': 'false';
+console.log(status1)
