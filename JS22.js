11fe959d54e8af108a5711744d774f98a1d5881f
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-22 14:21:42 +0530

    Add files via upload

---
+++ JS22.js
@@ -0,0 +1,22 @@
+//Functions in JavaScript
+//1
+function add(a,b)
+{
+    console.log(2+8);
+}
+add();
+
+//2
+//with perameter
+function addString(fname,lname)
+{
+    console.log(`FirstName:${fname} LastName:${lname}`);
+}
+addString(`chanchal`,`aklare`);
+
+//3
+function addIntString(name, age)
+{
+    console.log(`Name:${name} Age:${age}`);
+}
+addIntString(`Priyal`,23);
