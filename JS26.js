d0f6b4861c7a79df576e5d83fe6ed84578ff876a
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-23 16:58:28 +0530

    Add files via upload

---
+++ JS26.js
@@ -0,0 +1,9 @@
+//write a js function to take p,t,r,as argument and return it's interest
+//p- price t-time r-rate 
+function interest(p,t,r)
+{
+    console.log(`Interest Rate:${p*t*r/100}`);
+    return p,t,r;
+}
+interest(2000,4,6);
+
