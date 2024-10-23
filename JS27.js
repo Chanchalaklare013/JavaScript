9928f6dc94e1c4d058f5764c72b80d4daa42488c
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-23 16:59:00 +0530

    Add files via upload

---
+++ JS27.js
@@ -0,0 +1,12 @@
+//write a js function without arguments to calculate area of triangle and return its area(print area using document.writeln)
+let b=20
+let h=40
+//let area;
+function areaOfTriangle(area)
+{
+     area=1/2*b*h;
+    console.log(`Area of Triangle:${area}`);
+    return area;
+}
+areaOfTriangle();
+
