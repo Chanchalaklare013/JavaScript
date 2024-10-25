eb1015efb006f53b6485724c4d4c79549087be04
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-25 11:49:14 +0530

    Add files via upload

---
+++ JS36.js
@@ -0,0 +1,7 @@
+//to merge 3 array which stores marks of 3 students for 5 subjects are //(its importent)spread operator 
+        //and for of loop to print all marks
+        let s1=[67,88,77,34,89];
+        let s2=[55,67,45,87,98];
+        let s3=[55,87,56,53,62];
+        let c=[...s1, ...s2, ...s3];
+        console.log(c);
