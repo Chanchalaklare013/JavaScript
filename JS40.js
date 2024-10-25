1ed2586a4446b8d7ea4cc447e91b1bf5d19f6089
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-25 11:57:32 +0530

    Add files via upload

---
+++ JS40.js
@@ -0,0 +1,53 @@
+// left shipt in bite operators
+//console.log(20<<1);//out put 10
+
+//right shift
+//console.log(5>>1)//output 10
+
+// COnditional Statement 
+//1. if-else
+//age=17;
+// if(age>18)
+// {
+//     console.log(`you can vote`);
+// }
+// else
+// {
+//     console.log(`you can't vote`)
+// }
+
+
+//2. if-else-if
+// if(age>18)
+// {
+//   console.log("vote")
+// }
+// else if(age>60)
+// {
+//     console.log("cannto vote")
+// }
+// else
+// {
+//     console.log("not eligible")
+// }
+
+
+//switch case
+ let num=3;
+switch(num)
+{
+    case 1:
+        console.log("A with 1")
+    break;
+    case 2:
+        console.log("B with 2");
+    break;
+    case 3:
+        console.log("C with 3");
+    break;
+    case 4:
+        console.log("D with 4");
+    break;
+    default:
+        console.log("not applicable")
+}
