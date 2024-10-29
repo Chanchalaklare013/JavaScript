7f9dff026f06dc4d517304ef3f9cf85e41ee3f2b
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-10-29 18:38:31 +0530

    Add files via upload

---
+++ JSLoops.js
@@ -0,0 +1,107 @@
+//Loops in JS
+//For loop
+// for(let i=1;i<=10;i++)
+// {
+//     console.log("chanchal");
+// }
+
+// for(let i=0;i<=6;i++)
+// {
+//     console.log(i);
+// }
+
+// for(let i=5;i>0;i--)
+// {
+//     console.log(i);
+// }
+
+// for(let i=0;i<=6;i++)
+// {
+//     if(i==4)
+//     {
+//         break;
+//     }
+//     else
+//     {
+//        console.log(i);
+//     }
+// }
+
+
+// for(let i=1;i<=4;i++)
+// {
+//     if(i==3)
+//     {
+//         continue;
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+// }
+
+//While Loop
+// let i=0;
+// while(i<=10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// let i=6;
+// while(i>=0)
+// {
+//     console.log(i);
+//     i--;
+// }
+
+// let i=0;
+// while(i<=6)
+// {
+//     if(i==4)
+//     {
+//         break;
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// let i=1;
+// while(i<5)
+// {
+//     //console.log("inside the loop");
+//     if(i==3)
+//     {
+//         i++
+//         continue;
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+//Do-While loop
+// let i=0;
+// do{
+//     console.log("pinu");
+//     i++
+// }
+// while(i<=10);
+
+// let i=5;
+// do{
+//     console.log(i)
+//     i--
+// }
+// while(i>0);
+
+
+
+
+
