a9e2ed502a36d6d28c94a780b2d5f9563d1f21cd
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-11-11 21:51:49 +0530

    Add files via upload

---
+++ JSFunctions.js
@@ -0,0 +1,64 @@
+
+// function solve()
+//  {
+//      console.log(`chanchal aklare`);
+//  }
+// solve();
+
+// function loops()
+// {
+//     for(let i=0;i<=10;i++)
+// {
+//     console.log(i);
+// }
+// }
+// loops();
+
+// function perameter(num)
+// {
+//     console.log(num);
+
+// }
+// perameter(10);
+ //First-order Function
+ /*const person={name:'chanchal',age:'24'}
+function getPropertyName(obj)
+{
+    //console.log(name)
+   return obj.age;
+    //console.log("hello")
+}
+getPropertyName();*/
+
+//double perameter function
+// function getAverage(num1,num2)
+// {
+//     let avg=(num1+num2)/2;
+//     console.log("Vaverage "+avg);
+// }
+// getAverage(3,70);
+
+
+//Return function in JAvaScript
+// function getsum(a,b,c)
+// {
+//     let sum=a+b+c;
+//     return sum;
+
+// }
+// getsum(4,4,4);
+
+
+// function getMyName(firstName, lastName)
+// {
+//     let fullName = firstName+" "+lastName;
+//     return fullName;
+// }
+
+// let fullName = getMyName("pinu", "aklare");
+// console.log("full Name",fullName);
+
+console.log(${&#128564})
+
+
+   
