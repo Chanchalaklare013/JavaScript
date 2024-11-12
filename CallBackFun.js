883750cdbf05c5117555ec3d627291145d9c9643
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-11-12 14:19:27 +0530

    Add files via upload

---
+++ CallBackFun.js
@@ -0,0 +1,14 @@
+function solve()
+{
+
+}
+
+//Anonymous Function in callback function 
+var x = function(a)
+{
+    console.log("hellooo..");
+     let s=a*3;
+     console.log(s)
+};
+x(5);
+console.log(x);
