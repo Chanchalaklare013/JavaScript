d43e76a3a0876128794a9b75218eced132f24b32
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-11-12 14:17:53 +0530

    Add files via upload

---
+++ ArrowFunMany.js
@@ -0,0 +1,16 @@
+var f1=()=>{
+    console.log("Arrow Function executed..");
+}
+f1();
+
+var f2=(massage)=>{
+    console.log(""+massage);
+}
+f2("wishing you");
+
+var f3=(massage)=>{
+    console.log(""+massage);
+    f2("start of Divya solanki")
+}
+
+f3("Happy Diwali...")
