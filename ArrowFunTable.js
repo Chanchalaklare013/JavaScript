0ca3f2fa84a390160adea840b3bdac5ff44d10a7
Chanchalaklare013 <aklarechanchal@gmail.com>
2024-11-12 14:18:55 +0530

    Add files via upload

---
+++ ArrowFunTable.js
@@ -0,0 +1,19 @@
+//Anonymous Function without perameter
+// let table=function()
+// {
+//     for(let i=1;i<=10;i++)
+//     {
+//         console.log(5*i)
+//     }
+// }
+// table();
+
+//with perameters
+let table=function(a)
+{
+    for(let i=1;i<=10;i++)
+    {
+        console.log(a*i)
+    }
+}
+table(6);
